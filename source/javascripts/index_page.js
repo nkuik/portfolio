e2a384a9e3914328161a73fe7189f1e9b5e2d8a7
Nathan Kuik <nathan.kuik@gmail.com>
2016-09-07 17:21:54 +0200

    Cards with latest projects, general fixes around site

--- source/javascripts/index_page.js
+++ source/javascripts/index_page.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
-});
+
+  // $("#contactForm").validate({
+  // rules: {
+  //     first_name: "required",
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     }
+  //   },
+
+    submitHandler: function(form) {
+      $.ajax({
+        dataType: 'jsonp',
+        url: "http://getsimpleform.com/messages/ajax?form_api_token=457fbb20f41eab9dc5dc36155a55acb4",
+            data: $("#contactForm").serialize()
+          }).done(function() {
+        //callback which can be used to show a thank you message
+        //and reset the form
+        window.location.href = "/confirm.html";
+      });
+    }
+  });
+
+// });
