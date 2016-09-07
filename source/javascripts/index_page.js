4a0776f7fc6b7499423e487ccb7109ebf8c958ed
Nathan Kuik <nathan.kuik@gmail.com>
2016-09-07 20:10:37 +0200

    Form is working

--- source/javascripts/index_page.js
+++ source/javascripts/index_page.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
 
-  // $("#contactForm").validate({
-  // rules: {
-  //     first_name: "required",
-  //     email: {
-  //       required: true,
-  //       email: true
-  //     }
-  //   },
+  $("#send-message").on("click", function(e) {
 
-    submitHandler: function(form) {
-      $.ajax({
-        dataType: 'jsonp',
-        url: "http://getsimpleform.com/messages/ajax?form_api_token=457fbb20f41eab9dc5dc36155a55acb4",
-            data: $("#contactForm").serialize()
-          }).done(function() {
-        //callback which can be used to show a thank you message
-        //and reset the form
-        window.location.href = "/confirm.html";
-      });
-    }
+    var firstName = $("#first_name").val();
+    var lastName = $("#last_name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    // console.log("This is my name " + firstName + " " + lastName);
+    // console.log("This is my email " + email);
+    // console.log("Here's my message: " + message);
+
+    $.ajax({
+    url: "https://formspree.io/nathan.kuik@gmail.com",
+    method: "POST",
+    data: {name: firstName + " " + lastName, email: email, message: message},
+    dataType: "json",
+      success: function(data) {
+        Materialize.toast('Your message was sent successfully!', 4500);
+        $("#contact-form").addClass("hidden");
+        $("#success-message").removeClass("hidden");
+        Materialize.showStaggeredList("#staggered-list")
+      },
+      error: function(jqXHR) {
+        Materialize.toast('Oops, something went wrong!', 4500);
+        alert(jqXHR.responseJSON);
+      },
+    });
+
   });
+});
 
-// });
