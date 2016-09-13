fe8956d20f8128b722a3f237121ba950193de532
Nathan Kuik <nathan.kuik@gmail.com>
2016-09-13 16:16:19 +0200

    gray wrappers contact form/footer, add angellist icon to footer, mobile navbar javascript change

--- source/javascripts/index_page.js
+++ source/javascripts/index_page.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
 
   $('.scrollspy').scrollSpy();
 
@@ -42,4 +47,3 @@ $(document).ready(function() {
 
   });
 });
-
