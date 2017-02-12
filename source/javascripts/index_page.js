3c116c86c4df140e1b1e21e28a028b3dade11565
Nathan Kuik <nathan.kuik@gmail.com>
2017-02-12 18:44:20 +0100

    remove search

--- source/javascripts/index_page.js
+++ source/javascripts/index_page.js
@@ -39,14 +39,5 @@ $(document).ready(function() {
         Materialize.toast('Oops, something went wrong! Try emailing me instead: nathan@nathankuik.com', 6500);
       },
     });
-
   });
 });
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-91846507-1', 'auto');
-ga('send', 'pageview');
