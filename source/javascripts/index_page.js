ed42ed671e9d1fa6188c22d04d4d4f73c34b2563
Nathan Kuik <nathan.kuik@gmail.com>
2017-02-12 18:31:16 +0100

    fix analytics

--- source/javascripts/index_page.js
+++ source/javascripts/index_page.js
@@ -41,17 +41,12 @@ $(document).ready(function() {
     });
 
   });
-
-// Fixed navbar
-  // $(function () {
-  //   $(window).scroll(function () {
-  //           // set distance user needs to scroll before we fadeIn navbar
-  //     if ($(this).scrollTop() > 80) {
-  //       $('.nav').addClass('nav-fixed');
-  //     } else {
-  //       $('.nav').removeClass('nav-fixed');
-  //     }
-  //   });
-  // });
-
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-91846507-1', 'auto');
+ga('send', 'pageview');
