e38dcea62343a0f54088bc1b2e74a7e191e0afb6
Nathan Kuik <nathan.kuik@gmail.com>
2018-05-13 22:26:38 +0200

    Add theme, dynamic commit message deployment script

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+THEME=$1
+
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -19,10 +21,16 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo -t $THEME
+
+echo "Generating commit message"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "$msg"
 
 echo "Pushing to gh-pages branch"
 git push upstream gh-pages --force
