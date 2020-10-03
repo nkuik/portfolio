5326384bc775d475d2eca87a94702a9f4ee248b2
Nathan Kuik <nkuik@toogoodtogo.com>
2020-10-03 16:44:46 +0200

    Initializing gh-pages branch

--- publish_to_gh_pages.sh
+++ publish_to_gh_pages.sh
@@ -15,7 +15,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -33,4 +33,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "$msg"
 
 echo "Pushing to gh-pages branch"
-git push upstream gh-pages --force
+git push origin gh-pages --force
