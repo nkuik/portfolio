97cf2912208e87ddfafa57e8abc28649e0cc7e63
Nathan Kuik <nkuik@toogoodtogo.com>
2021-01-13 17:55:53 +0100

    Try custom action

---
+++ actions/build-site/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+
+git config --global user.name $INPUT_GITHUB_USER
+git config --global user.email "nathan@nathankuik.com"
+
+pushd $(echo $GITHUB_REPOSITORY | awk -F'/' '{print $2}')
+git checkout $GITHUB_HEAD_REF
+git submodule update --init
+
+hugo version
+hugo
+
+git add .
+git commit -m "Build site for $GITHUB_HEAD_REF"
+git push -f origin $GITHUB_HEAD_REF
